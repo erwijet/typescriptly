c002004f80d39820d00e6c2995b8e99b985f0cdb
Tyler Holewinski <tyler.holewinski@bryx.com>
2024-01-18 15:05:35 -0500

    feat(str): add support for capitalize and formalize `TslyStr` class

--- packages/@tsly/core/src/string.test.ts
+++ packages/@tsly/core/src/string.test.ts
@@ -1,24 +1,34 @@
-import { escapeRegex, sliceStrTo, maybeParseInt, maybeParseFloat } from "./string";
+import { str } from "./string";
 
 test("escape regex", () => {
-    expect(new RegExp("$50").test("$50")).toEqual(false);
-    expect(new RegExp(escapeRegex("$50")).test("$50")).toEqual(true);
+  expect(new RegExp("$50").test("$50")).toEqual(false);
+  expect(new RegExp(str("$50").escapeRegex().take()).test("$50")).toEqual(true);
 });
 
 test("sliceStrTo", () => {
-    expect(sliceStrTo("apple.banana.orange.kiwi", ".")).toEqual("apple.");
-    expect(sliceStrTo("apple.banana.orange.kiwi", ".", 2)).toEqual("apple.banana.orange.");
-    expect(sliceStrTo("apple.banana.orange.kiwi", ".", 100)).toEqual("apple.banana.orange.kiwi");
+  expect(str("apple.banana.orange.kiwi").sliceTo(".").take()).toEqual("apple.");
+  expect(str("apple.banana.orange.kiwi").sliceTo(".", 2).take()).toEqual(
+    "apple.banana.orange."
+  );
+  expect(str("apple.banana.orange.kiwi").sliceTo(".", 100).take()).toEqual(
+    "apple.banana.orange.kiwi"
+  );
 });
 
 test("maybe parse int", () => {
-    expect(maybeParseInt("123")).toEqual(123);
-    expect(maybeParseInt("1010", 2)).toEqual(10);
-    expect(maybeParseInt("abc")).toBeNull();
+  expect(str("123").maybeParseInt()).toEqual(123);
+  expect(str("1010").maybeParseInt(2)).toEqual(10);
+  expect(str("abc").maybeParseInt()).toBeNull();
 });
 
 test("maybe parse float", () => {
-    expect(maybeParseFloat("3.14")).toEqual(3.14);
-    expect(maybeParseFloat("123")).toEqual(123);
-    expect(maybeParseFloat("abc")).toBeNull();
+  expect(str("3.14").maybeParseFloat()).toEqual(3.14);
+  expect(str("123").maybeParseFloat()).toEqual(123);
+  expect(str("abc").maybeParseFloat()).toBeNull();
+});
+
+test("capitalize", () => {
+  expect(str("hello, world").capitalize().take()).toEqual("Hello, world");
+  expect(str("HELLO, WORLD").capitalize().take()).toEqual("Hello, world");
+  expect(str("hello, WORLD").capitalize().take()).toEqual("Hello, world");
 });
--- packages/@tsly/core/src/string.ts
+++ packages/@tsly/core/src/string.ts
@@ -1,83 +1,117 @@
-/**
- * Given a string that contains regex commands, escape those characters to match as literals with regex.
- *
- * @example
- * ```ts
- * new RegExp("$50").test("$50"); // false
- * new RegExp(escapeRegex("$50")).test("$50"); // true
- * ```
- *
- * ?> special thanks to mary.strodl@bryx.com and https://stackoverflow.com/a/3561711
- */
-export function escapeRegex(s: string): string {
-  return s.replace(
-    /[\-\[\]\/\{}\(\)\*\+\?\.\\\^\$\|]/g,
-    "\\$&"
-  ); /* eslint-disable-line no-useless-escape */
+export function str(s: string): TslyStr {
+  return new TslyStr(s);
 }
 
-/**
- * Get a substring of the given string `str` from the origin of the string to the first or nth occurance of the given `to` string
- *
- * @example
- * ```ts
- * const str = 'apple.banana.orange.kiwi';
- * sliceStrTo(str, '.'); // 'apple.'
- * sliceStrTo(str, '.', 2); // 'apple.banana.orange.'
- * ```
- */
-export function sliceStrTo(str: string, to: string, nth = 0): string {
-  const sliced = str.slice(0, str.indexOf(to) + to.length);
+class TslyStr extends String {
+  escapeRegex(): TslyStr {
+    return str(
+      this.replace(/[\-\[\]\/\{}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
+    ); /* eslint-disable-line no-useless-escape */
+  }
 
-  if (sliced == "") return str; // no match
-  if (nth == 0) return sliced;
-  else return sliced + sliceStrTo(str.slice(sliced.length), to, nth - 1);
+  sliceTo(to: string, nth = 0): TslyStr {
+    const sliced = this.slice(0, this.indexOf(to) + to.length);
+
+    if (sliced == "") return this; // no match
+    if (nth == 0) return str(sliced);
+    else
+      return str(sliced + str(this.slice(sliced.length)).sliceTo(to, nth - 1));
+  }
+
+  capitalize(): TslyStr {
+    if (this.take() == "") return this;
+    return str(this.at(0)!.toUpperCase() + this.slice(1).toLowerCase());
+  }
+
+  maybeParseInt(radix?: number): number | null {
+    const parsed = parseInt(this.take(), radix);
+    return isNaN(parsed) ? null : parsed;
+  }
+
+  maybeParseFloat(): number | null {
+    const parsed = parseFloat(this.take());
+    return isNaN(parsed) ? null : parsed;
+  }
+
+  take(): string {
+    return this.slice(0);
+  }
 }
 
-/**
- * Parses a string into an integer, returning `null` instead of `NaN` if parsing fails
- *
- * @example
- * ```ts
- * // returns 10
- * maybeParseInt('10');
- * ```
- *
- * @example
- * ```ts
- * maybeParseInt('abc');
- * // returns null
- * ```
- *
- * @param str - The string to parse.
- * @param radix - The radix used to parse the string. Defaults to 10.
- * @returns The parsed integer, or null if parsing fails.
- *
- */
-export function maybeParseInt(str: string, radix?: number): number | null {
-  const parsed = parseInt(str, radix);
-  return isNaN(parsed) ? null : parsed;
-}
+// /**
+//  * Given a string that contains regex commands, escape those characters to match as literals with regex.
+//  *
+//  * @example
+//  * ```ts
+//  * new RegExp("$50").test("$50"); // false
+//  * new RegExp(escapeRegex("$50")).test("$50"); // true
+//  * ```
+//  *
+//  * ?> special thanks to mary.strodl@bryx.com and https://stackoverflow.com/a/3561711
+//  */
+// export function escapeRegex(s: string): string {
+//   return s.replace(
+//     /[\-\[\]\/\{}\(\)\*\+\?\.\\\^\$\|]/g,
+//     "\\$&"
+//   ); /* eslint-disable-line no-useless-escape */
+// }
 
-/**
- * Parses a string into a floating-point number, returning null instead of `NaN` if parsing fails.
- *
- * @example
- * ```ts
- * maybeParseFloat('3.14');
- * // returns 3.14
- * ```
- *
- * @example
- * ```ts
- * maybeParseFloat('abc');
- * // returns null
- * ```
- *
- * @param str - The string to parse.
- * @returns The parsed number, or null if parsing fails.
- */
-export function maybeParseFloat(str: string): number | null {
-  const parsed = parseFloat(str);
-  return isNaN(parsed) ? null : parsed;
-}
+// /**
+//  * Get a substring of the given string `str` from the origin of the string to the first or nth occurance of the given `to` string
+//  *
+//  * @example
+//  * ```ts
+//  * const str = 'apple.banana.orange.kiwi';
+//  * sliceStrTo(str, '.'); // 'apple.'
+//  * sliceStrTo(str, '.', 2); // 'apple.banana.orange.'
+//  * ```
+//  */
+// export function sliceStrTo(str: string, to: string, nth = 0): string {}
+
+// /**
+//  * Parses a string into an integer, returning `null` instead of `NaN` if parsing fails
+//  *
+//  * @example
+//  * ```ts
+//  * // returns 10
+//  * maybeParseInt('10');
+//  * ```
+//  *
+//  * @example
+//  * ```ts
+//  * maybeParseInt('abc');
+//  * // returns null
+//  * ```
+//  *
+//  * @param str - The string to parse.
+//  * @param radix - The radix used to parse the string. Defaults to 10.
+//  * @returns The parsed integer, or null if parsing fails.
+//  *
+//  */
+// export function maybeParseInt(str: string, radix?: number): number | null {
+//   const parsed = parseInt(str, radix);
+//   return isNaN(parsed) ? null : parsed;
+// }
+
+// /**
+//  * Parses a string into a floating-point number, returning null instead of `NaN` if parsing fails.
+//  *
+//  * @example
+//  * ```ts
+//  * maybeParseFloat('3.14');
+//  * // returns 3.14
+//  * ```
+//  *
+//  * @example
+//  * ```ts
+//  * maybeParseFloat('abc');
+//  * // returns null
+//  * ```
+//  *
+//  * @param str - The string to parse.
+//  * @returns The parsed number, or null if parsing fails.
+//  */
+// export function maybeParseFloat(str: string): number | null {
+//   const parsed = parseFloat(str);
+//   return isNaN(parsed) ? null : parsed;
+// }
